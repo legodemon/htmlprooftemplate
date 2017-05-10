5f9e0dfa482f4616b21343f82c81a6b59928bf64
Konstantin Shushanskii <konstantin.shushanskii@alpari.ru>
2017-05-10 15:25:22 +0300

    menu page-proof

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-        /*
-         $(".nav li").hover(function() {
-            $(this).addClass('hover');
-            }, function() {
-            $(this).removeClass('hover');
-            });
-             */
+    [].forEach.call(document.querySelectorAll('.nav li'), function (element) {
+        element.addEventListener('mouseenter', function(){
+            element.classList.add('hover');
+        });
 
-        const elements = document.querySelectorAll('.nav li');
-
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].onmouseover = function () {
-                this.classList.add('hover');
-            };
-
-            elements[i].onmouseout = function () {
-                this.classList.remove('hover');
-            }
-        }
-    }
-);
+        element.addEventListener('mouseleave', function(){
+            element.classList.remove('hover')
+        });
+    });
+});
